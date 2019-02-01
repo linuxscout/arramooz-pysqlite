efcc127ffb27942db383fb73b11b6d92e59c0450
linuxscout <taha_zerrouki@hotmail.com>
2019-02-01 23:51:07 +0100

    update database
    - update verb stamp

--- arramooz/arabicdictionary.py
+++ arramooz/arabicdictionary.py
@@ -98,9 +98,9 @@ class ArabicDictionary:
         # the stamp is used as a first level of indexing, especially
         # for verbs
         # the stamp pattern is used to create the word stamp
-        self.stamp_pat = re.compile(u"[%s%s%s%s%s%s%s%s%s]"% (araby.ALEF, 
+        self.stamp_pat = re.compile(u"[%s%s%s%s%s%s%s%s%s%s]"% (araby.ALEF, 
         araby.YEH, araby.HAMZA, araby.ALEF_HAMZA_ABOVE, araby.WAW_HAMZA,
-         araby.YEH_HAMZA, araby.WAW, araby.ALEF_MAKSURA, araby.SHADDA), 
+         araby.YEH_HAMZA, araby.WAW, araby.ALEF_MAKSURA, araby.SHADDA, araby.ALEF_MADDA), 
          re.UNICODE)
 
     def __del__(self):
